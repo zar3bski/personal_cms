2bdf7b9eb298bf01645c54f649379a441f500dfd
ZarebskiDavid <zarebskidavid@gmail.com>
2019-05-25 16:59:27 +0200

    feat:robots.txt

--- cms_src/website/urls.py
+++ cms_src/website/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.http import HttpResponse
 
 from . import views
 
@@ -6,4 +7,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('browse/', views.Browse.as_view(), name='browse'),
     path('education/', views.Education.as_view(), name='education'),
+    path('read/<str:path>/<int:article_id>', views.Reader.as_view(), name="reader"), 
+    path('robots.txt', lambda x: HttpResponse("User-Agent: *\nDisallow: /admin", content_type="text/plain"), name="robots_file"),
 ]
