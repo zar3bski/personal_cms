26b35e6772991ada60e6b320aa804c0bca3ba4ee
ZarebskiDavid <zarebskidavid@gmail.com>
2019-06-03 04:07:47 +0200

    feat:data for development 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # personal_cms
+
+default test credentials `john` : `478951236a` 
--- cms_src/entrypoint.sh
+++ cms_src/entrypoint.sh
@@ -14,6 +14,8 @@ fi
 python manage.py flush --no-input
 python manage.py migrate
 
+python manage.py loaddata ../test/data_dev.yaml 
+
 python manage.py collectstatic --no-input --clear
 
 exec "$@"
