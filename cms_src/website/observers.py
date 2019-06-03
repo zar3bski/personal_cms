fb3eeb1eddf3f0ec5095140c4a1c19d94942c846
ZarebskiDavid <zarebskidavid@gmail.com>
2019-06-03 12:17:31 +0200

    feat:new user automatically added to Person

--- cms_src/website/observers.py
+++ cms_src/website/observers.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
-from .models import Article_category, Photo_category
+from .models import Article_category, Photo_category, Person
+from django.contrib.auth.models import User
 from django.core.cache import cache
 
 @receiver(post_save, sender=Photo_category)
@@ -9,3 +10,11 @@ from django.core.cache import cache
 @receiver(post_delete, sender=Article_category)
 def refresh_cached_category(sender, instance, using, **kwargs):
     cache.set('{}'.format(type(instance).__name__), type(instance).objects.all())
+
+
+@receiver(post_save, sender=User)
+def add_new_user_to_persons(sender, instance, **kwargs): 
+    p = Person.objects.get_or_create(
+        first_name = instance.first_name, 
+        last_name  = instance.last_name)
+    
