f35fbbfbc85e9baf3c214bf49911d781d6ba1cfa
ZarebskiDavid <zarebskidavid@gmail.com>
2019-10-17 21:52:58 +0200

    WIP improving backend support

--- cms_src/entrypoint.prod.sh
+++ cms_src/entrypoint.prod.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "mariadb" ]
-then
-    echo "Waiting for mariadb..."
+while ! nc -z $SQL_HOST $SQL_PORT; do
+  sleep 0.1
+done
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "MariaDB started"
-fi
+echo "DB started"
 
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
